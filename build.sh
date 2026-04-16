476978864e122f3e7070ac29cfe3976ab5145535
Ostapenko Alexey <dolu.bl@gmail.com>
2026-04-16 17:57:01 +0300

    [cmake] Переход на CMakePresets.

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-mkdir -p build
-
-# Configure
-CC=gcc-14 CXX=g++-14 cmake -B build \
-    -DENABLE_TESTING=ON \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=cmake/conan_provider.cmake
-
-# Build
-cmake --build build -j$(($(nproc) - 1))
-
-# Test
-ctest --test-dir build --output-on-failure
+cmake --preset default
+cmake --build --preset default
+ctest --preset default
